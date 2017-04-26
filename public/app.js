2be3d18d2d23b3a6787a06e8e9ebfc08f461009a
Grégory <claudejurado@orange.fr>
2017-04-26 14:43:09 +0200

    on crée des objets aléatoires dans le tableau a une position aléatoire et on leur a attribué une couleur propre

--- public/app.js
+++ public/app.js
@@ -1,18 +1,32 @@
-
 for (var i = 0; i < 20; i++) {
 	$("#table").append('<tr data-row="'+i+'"></tr>');
 	for (var j = 0; j < 20; j++) {
 		$("#table tr:last-child").append('<td data-row="'+i+'"data-column="'+j+'" ></td>');
 	}
-}
+};
+
+
+
 $("#Générer").click(function(){
 	   $.ajax({
 	        url:'http://192.168.1.137:3000',
-	        type: "post",
-	        data: {}
+	        type: "POST",
+	        data: {
+	        	test:"test",
+	        	color:"color",
+	        	val:"val",
+	        	X:"posX",
+	        	Y:"PosY",
+	        }
 	    })
 	    .done(function(res) {
+	    	var rustine=res.val;
+	    	$("td[data-row='"+res.posX+"'][data-column='"+res.posY+"']").append(res.val);
+	    	$("td[data-row='"+res.posX+"'][data-column='"+res.posY+"']").css("background-color",res.color);
+	        console.log(res.color);
 	        console.log(res);
+	        console.log("data-row='"+res.posX+"'")
+	        console.log(rustine);
 	    });
 });
 		
--- server.js
+++ server.js
@@ -1,61 +1,72 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
-var randcell;
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-var objects =[plant, herbivore, carnivore, stone];
-var plant ={
+var objects =[
+{
 	val: "P",
+	color: "green",
 	name: "plant",
 	force: 1,
 	cut:1 
 
-};
-var herbivore={
+},
+{
 	val: "H",
+	color: "blue",
 	name: "herbivorus",
 	force: 2,
 	cut: 1
 
-};
-var carnivore={
+},
+{
 	val:"C",
+	color: "red",
 	name: "carnivorus",
 	force : 3,
 	cut:1
 
-};
-var stone={
+},
+{
 	val: "S",
+	color: "grey",
 	name: "stone",
 	force: 4,
 	cut: 1
 
-};
+}
+];
 
  app.get("/", function(req, res){
  	res.sendfile("public/index.html")
 
  });
-app.post("/", function(req,res){
-	res.send(createObject)
-})
 app.use(express.static('public'));
 
 app.listen(3000, function(){
 	console.log("yipikay!")
 });
 
-function createObject (){
-	ord = MathRound(MathRandom()*20);
-	abs = MathRound(MathRandom()*20);
-	randcell = $("td[data-row='"+ord+"'][data-column ='"+abs+"']")
-	
-	randObject= MathRound(MathRandom()*4);
-	console.log(randcell.append(objects[randObject].val))
+app.post("/", function(req,res){
+	var randcell;
+	var ord;
+	var abs;
+	var sended;
+	var randObject;
+	ord = Math.round(Math.random()*20);
+	abs = Math.round(Math.random()*20);
+	aleat= Math.round(Math.random()*3);
+	selected= objects[aleat];
+	 sended={
+	 	color: selected.color,
+	 	test: selected,
+	 	val: selected.val,
+	 	posX: ord,
+	 	posY: abs,
+	 }
+	res.send(sended)
+})
 
-
-
-}
